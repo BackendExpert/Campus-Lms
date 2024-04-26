4c78bb68dd1c9bd1d9d33fcdc9372a8e83c5ea55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-26 19:39:39 +0530

    Update AddBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/AddBook.jsx
+++ client/src/comportments/Books/AddBook.jsx
@@ -13,6 +13,16 @@ const AddBook = () => {
         return (
             <div className="bg-white rounded-2xl py-8 px-10 mt-6 shadow-md">
                 <h1 className="font-semibold text-gray-500 text-xl">Add New Book</h1>
+                <div className="my-4">
+                    <form>
+                        <div className="lg:grid grid-cols-3">
+                            <div className="my-2">
+                                <label htmlFor="">ISBN Number : </label>
+                                <input type="text" name="" id="" required className="w-full border border-blue-500 h-12 rounded shadow-md"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }

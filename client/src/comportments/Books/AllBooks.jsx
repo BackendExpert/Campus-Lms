d0182f85e93d76746f6287ca0959782cfbeea546
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-27 07:41:23 +0530

    Update AllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/AllBooks.jsx
+++ client/src/comportments/Books/AllBooks.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 const AllBooks = () => {
   return (
-    <div>AllBooks</div>
+    <div>
+        <h1>All Books</h1>
+
+        <div className="">
+            <table class="table-auto">
+            <thead>
+                <tr>
+                    <th>Song</th>
+                    <th>Artist</th>
+                    <th>Year</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                </tr>
+                <tr>
+                    <td>Witchy Woman</td>
+                    <td>The Eagles</td>
+                    <td>1972</td>
+                </tr>
+                <tr>
+                    <td>Shining Star</td>
+                    <td>Earth, Wind, and Fire</td>
+                    <td>1975</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
 
 export default AllBooks
+

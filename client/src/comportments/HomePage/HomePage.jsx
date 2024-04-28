e4aeb65d278bcb1bdc8bdd1a0d4dde8818501557
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:26:34 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -107,7 +107,9 @@ const HomePage = () => {
                 </div>         
             </div>
             <div className="text-center">
+                <Link>
                     <button className='bg-gray-300 py-2 px-8 rounded shadow-md duration-500 hover:bg-gray-400'>Browse more books</button>    
+                </Link>
             </div>       
         </div>
         <Footer />

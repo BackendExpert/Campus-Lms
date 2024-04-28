fd0be43b601dedfd8cb10ea8401857ba64dfca8a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:53:47 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Dashboard/SummaryDash.jsx
+++ client/src/comportments/Dashboard/SummaryDash.jsx
@@ -12,6 +12,8 @@ const SummaryDash = () => {
     const EmailUser = secureLocalStorage.getItem("login2");
 
     const [CountBooks, SetCoutBooks] = useState(0)
+    const [CountBorrowBooks, SetCountBorrowBooks] = useState(0)
+    const [CountSelectedBooks, SetCountSelectedBooks] = useState(0)
     
     useEffect(() => {
         const fetchData = async () => {
@@ -36,7 +38,7 @@ const SummaryDash = () => {
 
     const dataCount = [
         {id: 1, name: "Books", link: "#", value: <CountUp end={CountBooks}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-purple-500"},
-        {id: 2, name: "Borrowed Books", link: "#", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-yellow-500"}, 
+        {id: 2, name: "Borrowed Books", link: "#", value: <CountUp end={CountBorrowBooks}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-yellow-500"}, 
         {id: 3, name: "My Borrowed", link: "#", value: <CountUp end={20}/>, icon: <Icons name="book" size="large"></Icons>, style: "text-green-500"},
         {id: 4, name: "Users", link: "#", value: <CountUp end={20}/>, icon: <Icons name="people" size="large"></Icons>, style: "text-green-500"}, 
                 

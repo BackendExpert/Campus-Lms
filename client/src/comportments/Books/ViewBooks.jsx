47858447c4e0a319ea86a8a539c1c567b5a594e2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:22:54 +0530

    Update ViewBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/ViewBooks.jsx
+++ client/src/comportments/Books/ViewBooks.jsx
@@ -15,7 +15,7 @@ const ViewBooks = () => {
     <div className="bg-gray-200 py-2 h-auto w-full">
       <div className="bg-white mx-6 my-10 px-12 py-16 rounded-2xl shadow-md">
         <Link>
-          <button className="text-blue-500 font-semibold py-2 px-4 rounded shadow-xl duration-500 hover:bg-blue-500 hover:text-white">Back</button>
+          <button className="text-blue-500 font-semibold py-2 px-4 rounded duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
         </Link>
       </div>
     </div>

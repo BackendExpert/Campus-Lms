639bc5561090051240f7c8c4d514322770938741
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:08:38 +0530

    Update SelectedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/SelectedBooks.jsx
+++ client/src/comportments/Books/SelectedBooks.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const SelectedBooks = () => {
+    const navigate = useNavigate() 
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    const [SelectedBooks, SetSelectedBooks] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8081/SelectedBooks')
+        .then(res => (SetSelectedBooks(res.data)))
+        .catch(err => console.log(err))
+    }, [])
+
   return (
     <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
         <h1 className='text-gray-500 text-2xl font-semibold my-4'>All Books</h1>

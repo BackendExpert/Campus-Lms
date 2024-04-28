60a8e99609ae8332b55c09ba837737a6a83b2d20
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:07:53 +0530

    Update SelectedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/SelectedBooks.jsx
+++ client/src/comportments/Books/SelectedBooks.jsx
@@ -2,7 +2,80 @@ import React from 'react'
 
 const SelectedBooks = () => {
   return (
-    <div>SelectedBooks</div>
+    <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
+        <h1 className='text-gray-500 text-2xl font-semibold my-4'>All Books</h1>
+
+        <div className="">
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Book ISBN
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Books Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Authors
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Status
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            BorrowBooks.map((books, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {books.ISBN_No}
+                                        </th>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {books.Name}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {books.author1}, {books.author2}, {books.author3}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {
+                                                (() => {
+                                                    if(books.status === "Borrow"){
+                                                        return (
+                                                            <p className="text-red-500 font-semibold">Borrowed</p>
+                                                        )
+                                                    }
+                                                })()
+                                            }
+                                            
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {
+                                                (() => {
+                                                    if(books.status === "Borrow"){
+                                                        return (
+                                                            <button className="py-2 px-8 text-blue-500 font-semibold shadow-md rounded duration-500 hover:bg-blue-500 hover:text-white">
+                                                                Return
+                                                            </button>
+                                                        )
+                                                    }
+                                                })()
+                                            }
+
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
   )
 }
 

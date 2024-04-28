764a3614748a03ea043c077011b0d4b937208966
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:04:13 +0530

    Update BorrowedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/BorrowedBooks.jsx
+++ client/src/comportments/Books/BorrowedBooks.jsx
@@ -46,7 +46,7 @@ const BorrowedBooks = () => {
                     </thead>
                     <tbody>
                         {
-                            AllBooks.map((books, index) => {
+                            BorrowBooks.map((books, index) => {
                                 return (
                                     <tr key={index}>
                                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">

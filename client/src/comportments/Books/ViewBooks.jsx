b93d043fe4fbb506b97971650aa92e2324fd61eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:19:57 +0530

    Update ViewBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/ViewBooks.jsx
+++ client/src/comportments/Books/ViewBooks.jsx
@@ -5,6 +5,11 @@ import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
 
 const ViewBooks = () => {
+  const navigate = useNavigate() 
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
+  
   return (
     <div className="bg-gray-200 py-2 h-auto w-full">
       <div className="bg-white mx-6 my-10 px-12 py-16 rounded-2xl shadow-md">ViewBooks</div>

8a3163dc935fd4675441328fb2c9e09f2a843b91
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-28 18:23:19 +0530

    Update ViewBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Books/ViewBooks.jsx
+++ client/src/comportments/Books/ViewBooks.jsx
@@ -9,8 +9,6 @@ const ViewBooks = () => {
   const RoleUser = secureLocalStorage.getItem("Login1");
   const EmailUser = secureLocalStorage.getItem("login2");
 
-
-
   return (
     <div className="bg-gray-200 py-2 h-auto w-full">
       <div className="bg-white mx-6 my-10 px-12 py-16 rounded-2xl shadow-md">

c575a4af7ddd45106262b2a5e9f19c4983a0d187
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-03 15:51:15 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -430,7 +430,7 @@ app.get('/AllUsers', (req, res) => {
 // my selected books
 app.get('/MySelected/:id', (req, res) => {
     const userEmail = req.params.id
-    const sql = "SELECT COUNT(ID) AS MySelectBK FROM books WHERE email = ?";
+    const sql = "SELECT COUNT(ID) AS MySelectBK FROM books WHERE selectEmail = ?";
   
     connection.query(sql, [userEmail], (error, results) => {
       if (error) {

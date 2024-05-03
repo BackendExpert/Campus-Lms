2d895bf707710e68ed8b200b028ea589e2011282
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-03 15:57:19 +0530

    Update Users.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Users/Users.jsx
+++ client/src/comportments/Users/Users.jsx
@@ -19,72 +19,80 @@ const Users = () => {
         .catch(err => console.log(err))
     }, [])
 
-  return (
-    <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
-        <h1 className='text-gray-500 text-2xl font-semibold my-4'>All Books</h1>
-
-        <div className="">
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                ID
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Username
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Email
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Role
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            AllUsers.map((users, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {users.ID}
-                                        </th>
-                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {users.username}
-                                        </th>
-                                        <td class="px-6 py-4">
-                                            {users.email}
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            {
-                                                (() => {
-                                                    if(users.role === "SuperAdmin"){
-                                                        return (
-                                                            <p className="text-red-500 font-semibold">SuperAdmin</p>
-                                                        )
+    if(RoleUser === "SuperAdmin"){
+        return (
+            <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
+                <h1 className='text-gray-500 text-2xl font-semibold my-4'>All Books</h1>
+        
+                <div className="">
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Username
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Role
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    AllUsers.map((users, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {users.ID}
+                                                </th>
+                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {users.username}
+                                                </th>
+                                                <td class="px-6 py-4">
+                                                    {users.email}
+                                                </td>
+                                                <td class="px-6 py-4">
+                                                    {
+                                                        (() => {
+                                                            if(users.role === "SuperAdmin"){
+                                                                return (
+                                                                    <p className="text-red-500 font-semibold">SuperAdmin</p>
+                                                                )
+                                                            }
+                                                            else if(users.role === "user"){
+                                                                return (
+                                                                    <p className="text-yellow-500 font-semibold">User</p>
+                                                                )
+                                                            }
+                                                        })()
                                                     }
-                                                    else if(users.role === "user"){
-                                                        return (
-                                                            <p className="text-yellow-500 font-semibold">User</p>
-                                                        )
-                                                    }
-                                                })()
-                                            }
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+          )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default Users

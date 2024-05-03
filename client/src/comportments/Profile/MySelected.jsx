14749262f24ff53288144c784035cac3a2b67383
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-03 16:08:15 +0530

    Update MySelected.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Profile/MySelected.jsx
+++ client/src/comportments/Profile/MySelected.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup';
+import axios from "axios";
 
 const MySelected = () => {
+    const navigate = useNavigate() 
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    
   return (
     <div>MySelected</div>
   )

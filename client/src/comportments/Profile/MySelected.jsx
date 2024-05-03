e04c73e955c4fb79cb1194c8999380e98347e955
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-03 16:18:50 +0530

    Update MySelected.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/comportments/Profile/MySelected.jsx
+++ client/src/comportments/Profile/MySelected.jsx
@@ -21,7 +21,97 @@ const MySelected = () => {
     
     if(RoleUser !== null && EmailUser !== null) {
         return (
-            <div>MySelected</div>
+            <div className='bg-white py-4 px-8 my-8 rounded-2xl shadow-md'>
+                <h1 className='text-gray-500 text-2xl font-semibold my-4'>All Books</h1>
+        
+                <div className="">
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Book ISBN
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Books Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Authors
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    MySelected.map((MyBk, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {MyBk.ISBN_No}
+                                                </th>
+                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {MyBk.Name}
+                                                </th>
+                                                <td class="px-6 py-4">
+                                                    {MyBk.author1}, {MyBk.author2}, {MyBk.author3}
+                                                </td>
+                                                <td class="px-6 py-4">
+                                                    {
+                                                        (() => {
+                                                            if(MyBk.status === "Available"){
+                                                                return (
+                                                                    <p className="text-green-500 font-semibold">Available</p>
+                                                                )
+                                                            }
+                                                            else if(MyBk.status === "Borrow"){
+                                                                return (
+                                                                    <p className="text-red-500 font-semibold">Borrowed</p>
+                                                                )
+                                                            }
+                                                            else if(MyBk.status === "Selected"){
+                                                                return (
+                                                                    <p className="text-yellow-500 font-semibold">Seleted</p>
+                                                                )
+                                                            }
+                                                        })()
+                                                    }
+                                                    
+                                                </td>
+                                                <td class="px-6 py-4">
+                                                    {
+                                                        (() => {
+                                                            if(MyBk.status === "Borrow"){
+                                                                return (
+                                                                    <button className="py-2 px-8 text-red-500 font-semibold shadow-md rounded duration-500 hover:bg-red-500 hover:text-white">
+                                                                        Return
+                                                                    </button>
+                                                                )
+                                                            }
+                                                            else{
+                                                                return (
+                                                                    <button className="py-2 px-8 text-blue-500 font-semibold shadow-md rounded duration-500 hover:bg-blue-500 hover:text-white">
+                                                                        Borrow
+                                                                    </button>
+                                                                )
+                                                            }
+                                                        })()
+                                                    }
+        
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
